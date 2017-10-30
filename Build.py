853addf61f9b7b26e0ba67177af2d045027266e7
Jamie Phan <21326604@student.uwa.edu.au>
2017-10-30 20:50:17 +0800

    docs

--- Build.py
+++ Build.py
@@ -2,4 +2,4 @@ from subprocess import call
 call(["git", "pull"])
 call(["git", "add", "--all"])
 call(["git", "commit", "-m", "docs"])
-call(["git", "push", "origin",  "master"])
+call(["git", "push", "origin",  "src"])
